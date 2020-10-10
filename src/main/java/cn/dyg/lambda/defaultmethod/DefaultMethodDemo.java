76e9d4ae3ce9f9225dc731a12738396384afb8ea
YinggangDong <believerD@aliyun.com>
2020-10-10 17:07:45 +0800

    doc：JAVA8新特性-接口默认方法相关参考网址

--- src/main/java/cn/dyg/lambda/defaultmethod/DefaultMethodDemo.java
+++ src/main/java/cn/dyg/lambda/defaultmethod/DefaultMethodDemo.java
@@ -3,6 +3,14 @@ package cn.dyg.lambda.defaultmethod;
 /**
  * DefaultMethodDemo 类是接口默认方法demo类
  *
+ * 参考内容：
+ * 1.Java8 在接口的变化：
+ *      https://blog.csdn.net/axuanqq/article/details/82773631
+ * 2.接口默认方法
+ *      https://blog.csdn.net/h294590501/article/details/80303722
+ * 3.JAVA8学习5-接口默认方法（default）
+ *      https://blog.csdn.net/z_yemu/article/details/89312788?utm_medium=distribute.pc_relevant.none-task-blog-title-4&spm=1001.2101.3001.4242
+ *
  * @author dongyinggang
  * @date 2020-10-10 16:58
  **/
