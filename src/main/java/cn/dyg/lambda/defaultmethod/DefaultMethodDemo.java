98824633e551424c8135ced24c7bcebeb754d766
YinggangDong <believerD@aliyun.com>
2020-10-12 10:33:41 +0800

    feat：JAVA8新特性-接口默认方法、静态方法的相关内容

--- src/main/java/cn/dyg/lambda/defaultmethod/DefaultMethodDemo.java
+++ src/main/java/cn/dyg/lambda/defaultmethod/DefaultMethodDemo.java
@@ -14,13 +14,30 @@ package cn.dyg.lambda.defaultmethod;
  * @author dongyinggang
  * @date 2020-10-10 16:58
  **/
-public class DefaultMethodDemo implements MyInterface{
+public class DefaultMethodDemo implements MyInterface {
+
 
     public static void main(String[] args) {
         DefaultMethodDemo defaultMethodDemo = new DefaultMethodDemo();
+        //1.接口的默认方法
         defaultMethodDemo.defaultMethod();
+        //2.接口被重写的默认方法
+        defaultMethodDemo.defaultMethodWaitOverride();
+        //3.接口的静态方法,只能通过接口直接调用,实现类并不能重写该方法
         MyInterface.staticMethod();
+        //4.实现类中有和接口静态方法重名的方法
+        //  如果实例声明时使用了MyInterface就会报错,因为找不到唯一的方法,
+        //  使用DefaultMethodDemo声明调用的实际是实现类自己的静态方法
+        defaultMethodDemo.staticMethod();
+        //5.实现类重写的普通方法
         defaultMethodDemo.ordinaryMethod();
+
+        //接口的全局变量
+        System.out.println("通过接口访问" + MyInterface.INTERFACE_STATIC_VARIABLE);
+        System.out.println("通过实现类访问" + DefaultMethodDemo.INTERFACE_STATIC_VARIABLE);
+        //虽然标红,但实际是可以通过实现类的实例来访问接口的全局变量的,但不提倡这样写
+        System.out.println("通过实现类实例访问" + defaultMethodDemo.INTERFACE_STATIC_VARIABLE);
+
     }
 
     /**
@@ -31,6 +48,25 @@ public class DefaultMethodDemo implements MyInterface{
      */
     @Override
     public void ordinaryMethod() {
-        System.out.println(this.getClass().getSimpleName()+"实现的ordinaryMethod");
+        System.out.println(this.getClass().getSimpleName() + "实现的ordinaryMethod");
+    }
+
+    /**
+     * staticMethod 方法是 接口实现类尝试重写接口的static方法
+     * <p>
+     * 会发现并不能实现重写,加上 @Override 注解编译器会直接报错
+     * "Method does not override method from its superclass"
+     *
+     * @author dongyinggang
+     * @date 2020/10/12 9:08
+     */
+//    @Override
+    public void staticMethod() {
+        System.out.println(this.getClass().getSimpleName() + "尝试重写静态方法");
+    }
+
+    @Override
+    public void defaultMethodWaitOverride() {
+        System.out.println(this.getClass().getSimpleName() + "重写了默认方法");
     }
 }
--- src/main/java/cn/dyg/lambda/defaultmethod/MyInterface.java
+++ src/main/java/cn/dyg/lambda/defaultmethod/MyInterface.java
@@ -9,7 +9,23 @@ package cn.dyg.lambda.defaultmethod;
 public interface MyInterface {
 
     /**
+     * 在interface里面的变量都是public static final 的
+     *
+     * 接口就是提供一种统一的协议, 而接口中的属性也属于协议中的成员。
+     * 它们是公共的,静态的,最终的常量。相当于全局常量。
+     *
+     * 尽管不写public static final,也是默认常量
+     */
+    String INTERFACE_STATIC_VARIABLE = "静态变量";
+
+    /**
      * defaultMethod 方法是 接口默认方法
+     *
+     * 接口的默认方法被引入的主要原因：
+     *    以前创建了一个接口，并且已经被大量的类实现。
+     *    如果需要再扩充这个接口的功能加新的方法，就会导致所有已经实现的子类需要重写这个方法。
+     *    如果在接口中使用默认方法就不会有这个问题。
+     *    所以从 JDK8 开始新加了接口默认方法，便于接口的扩展。
      *
      * @author dongyinggang
      * @date 2020/10/10 16:53
@@ -19,7 +35,25 @@ public interface MyInterface {
     }
 
     /**
+     * defaultMethodWaitOverride 方法是 等待重写的接口默认方法
+     *
+     * @author dongyinggang
+     * @date 2020/10/12 9:47
+     */
+    default void defaultMethodWaitOverride(){
+        System.out.println("等待重写的默认方法");
+    }
+
+    /**
      * staticMethod 方法是 接口静态方法
+     *
+     * 在Java 8中，在接口中添加静态方法带来了一个限制 ：这些方法不能由实现它的类继承。
+     * 这样做是有道理的，因为一个类可以实现多个接口。如果2个接口具有相同的静态方法，
+     * 它们都将被继承，编译器就不知道要调用哪个接口。
+     *
+     * 参考：
+     * 关于在java 8中，为什么不能调用当前类正在实现的接口的静态方法的解释
+     *      https://blog.csdn.net/u012580143/article/details/81217732
      *
      * @author dongyinggang
      * @date 2020/10/10 16:53
