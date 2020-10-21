9ca9af71b3c0e12afc7079c411733135ab6a0d4b
YinggangDong <believerD@aliyun.com>
2020-10-21 08:10:48 +0800

    feat：JAVA8新特性-方法引用的拆分

---
+++ src/main/java/cn/dyg/lambda/doublecolon/Constructor.java
@@ -0,0 +1,64 @@
+package cn.dyg.lambda.doublecolon;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Supplier;
+
+/**
+ * Constructor 类是 构造函数的方法引用形式
+ *
+ * @author dongyinggang
+ * @date 2020-10-20 18:49
+ **/
+public class Constructor {
+
+    /**
+     * constructor 方法是 调用构造函数
+     *
+     * @author dongyinggang
+     * @date 2020/10/10 15:14
+     */
+    static void constructor(){
+        System.out.println("4.调用构造函数：");
+        List<Person> roster = Person.createRoster();
+        for (Person p : roster) {
+            p.printPerson();
+        }
+        //1.通过匿名内部类实现get()方法,调用构造函数
+        Set<Person> rosterSet = transferElements(roster, new Supplier<Set>(){
+            @Override
+            public Set get() {
+                return new HashSet<>();
+            }
+        });
+        //2.通过lambda表达式实现get()方法,调用构造函数
+        Set<Person> rosterSetLambda = transferElements(roster,()->new HashSet<>());
+        //3.简化lambda表达式实现get()方法,调用构造函数
+        Set<Person> rosterSimple = transferElements(roster,HashSet::new);
+        System.out.println("赋值完成：");
+        for (Person person : rosterSimple) {
+            person.printPerson();
+        }
+    }
+
+    /**
+     * transferElements 方法是 将一个collection的元素放到新的collection中去
+     *
+     * @param sourceCollection 源集合
+     * @param collectionFactory 集合工厂
+     * @return DEST的集合对象
+     * @author dongyinggang
+     * @date 2020/10/20 18:52
+     */
+    private static <T, SOURCE extends Collection<T>, DEST extends Collection<T>>
+    DEST transferElements(SOURCE sourceCollection,Supplier<DEST> collectionFactory) {
+        //通过get方法创建一个新的集合对象
+        DEST result = collectionFactory.get();
+        //将源集合中的元素add进新的集合对象中
+        result.addAll(sourceCollection);
+        //将新的集合对象放回
+        return result;
+    }
+}
---
+++ src/main/java/cn/dyg/lambda/doublecolon/ParticularTypeInstanceMethod.java
@@ -0,0 +1,59 @@
+package cn.dyg.lambda.doublecolon;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * ParticularTypeInstanceMethod 类是 特定类型任意对象的实例方法的方法引用
+ *
+ * @author dongyinggang
+ * @date 2020-10-20 18:47
+ **/
+public class ParticularTypeInstanceMethod {
+
+    /**
+     * particularTypeInstanceMethod 方法是 调用特定类型任意对象的实例方法
+     *
+     * @author dongyinggang
+     * @date 2020/10/10 11:29
+     */
+    static void particularTypeInstanceMethod() {
+        System.out.println("3.调用特定类型任意对象的实例方法:");
+        //官方文档示例
+        String[] stringArray = {"Barbara", "James", "Mary", "John",
+                "Patricia", "Robert", "Michael", "Linda"};
+        Arrays.sort(stringArray, String::compareToIgnoreCase);
+
+        List<Person> roster = Person.createRoster();
+        for (Person p : roster) {
+            p.printPerson();
+        }
+        Person[] rosterAsArray = roster.toArray(new Person[roster.size()]);
+        /**
+         * 刚开始考虑调用的方法是 compareByAgeNonStatic(Person a, Person b)
+         * 有两个参数，结果发现会出现编译错误，提示在静态上下文中调用了非静态变量
+         *
+         * 看了一下官方文档中使用的compareToIgnoreCase方法，发现其只有一个入参，
+         * 然后就给 compareByAgeNonStatic 写了一个只有一个参数的重载方法，发现可以正常调用
+         *
+         * 虽然官方文档说着可以调用特定类型任意对象的实例方法，但实际是有限制的，并不能像静态方法一样，
+         * 可以有多个入参
+         *
+         * 疑问：
+         * 1. 这里感觉理解有问题，Arrays.sort的比较器调用方法为静态时可以有两个参数的原因
+         * 实际是因为作为静态方法，他不需要通过类的实例调用，可以通过类直接进行调用，而实例方法必须通过实例调用，
+         * 因此形式为一个对象调用来和另一个对象比较
+         *
+         */
+        Arrays.sort(rosterAsArray, Person::compareByAgeNonStatic);
+        //和上方等价
+        Arrays.sort(rosterAsArray, (a, b) -> a.compareByAgeNonStatic(b));
+        //如果调用 compareByAgeNonStatic(a,b) 实际是调用特定对象的实例方法
+        Arrays.sort(rosterAsArray, (a, b) -> new Person().compareByAgeNonStatic(a, b));
+        System.out.println("排序完成：");
+        for (Person person : rosterAsArray) {
+            person.printPerson();
+        }
+    }
+
+}
