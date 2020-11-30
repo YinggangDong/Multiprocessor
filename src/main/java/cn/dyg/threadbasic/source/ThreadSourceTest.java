2ce41c866ad7c104572df81834ef5fc0e2c82830
YinggangDong <believerD@aliyun.com>
2020-11-30 15:18:33 +0800

    feat：Thread类解析

---
+++ src/main/java/cn/dyg/threadbasic/source/ThreadSourceTest.java
@@ -0,0 +1,28 @@
+package cn.dyg.threadbasic.source;
+
+/**
+ * ThreadSourceTest 类是 Thread 类常用方法解析
+ *
+ * @author dongyinggang
+ * @date 2020-11-30 10:46
+ **/
+public class ThreadSourceTest {
+
+    public static void main(String[] args) {
+        noArgsConstructor();
+    }
+
+    /**
+     * noArgsConstructor 方法是 无参构造
+     * 基本无意义
+     *
+     * @author dongyinggang
+     * @date 2020/11/30 15:17
+     */
+    public static void noArgsConstructor(){
+        Thread thread = new Thread();
+        System.out.println(thread.getName());
+        System.out.println("id:"+thread.getId());
+        thread.start();
+    }
+}
