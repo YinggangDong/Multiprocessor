44e6682f18bfce45ee0b94b6771a5323341bc7fb
YinggangDong <believerD@aliyun.com>
2020-12-01 20:06:12 +0800

    feat：Thread类init方法初始化线程时的命名解析

--- src/main/java/cn/dyg/threadbasic/source/ThreadSourceTest.java
+++ src/main/java/cn/dyg/threadbasic/source/ThreadSourceTest.java
@@ -11,13 +11,17 @@ import cn.dyg.threadbasic.create.ImplRunnable;
 public class ThreadSourceTest {
 
     public static void main(String[] args) {
-//        constructor();
-        threadName();
+        constructor();
+//        threadName();
     }
 
     /**
-     * constructor 方法是 构造方法
-     * 基本无意义
+     * constructor 方法是 常见构造方法
+     * 1.无参构造
+     * 2.指定线程名称的线程
+     * 3.指定 runnable 实现类的线程
+     * 4.指定 runnable 和线程名称的线程
+     * 调用方法实际是调用了 init 方法
      *
      * @author dongyinggang
      * @date 2020/11/30 15:17
@@ -25,18 +29,23 @@ public class ThreadSourceTest {
     public static void constructor(){
         //1.无参构造
         Thread thread = new Thread();
+        System.out.println(thread.getName());
         //2.指定线程名称的线程
         thread = new Thread("t-1");
 
         Runnable runnable = new ImplRunnable("r-1");
         //3.指定 runnable 实现类的线程
         thread = new Thread(runnable);
+        System.out.println(thread.getName());
         //4.指定 runnable 和线程名称的线程
         thread = new Thread(runnable,"t-2");
+
     }
 
     /**
      * threadName 方法是 设置线程名称的两种方式
+     * 注：
+     * 若没有指定线程名称,会使用"Thread-x"(x指线程初始数量,是 Thread 类的 threadInitNumber)
      *
      * @author dongyinggang
      * @date 2020/12/1 18:14
@@ -61,6 +70,12 @@ public class ThreadSourceTest {
         }
     }
 
+    /**
+     * outputThreadName 方法是 输出线程名称的方法
+     *
+     * @author dongyinggang
+     * @date 2020/12/1 19:20
+     */
     private static void outputThreadName(){
         int i = 0;
         int times = 30;
