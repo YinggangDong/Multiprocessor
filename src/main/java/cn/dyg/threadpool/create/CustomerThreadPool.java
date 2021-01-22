329bcdc04e7d6143049b12338e349c109b2e7c2f
YinggangDong <believerD@aliyun.com>
2021-01-22 19:31:39 +0800

    feat：修改自定义线程的相关内容

--- src/main/java/cn/dyg/threadpool/create/CustomerThreadPool.java
+++ src/main/java/cn/dyg/threadpool/create/CustomerThreadPool.java
@@ -3,7 +3,9 @@ package cn.dyg.threadpool.create;
 import cn.dyg.threadpool.factory.MyFactory;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -17,9 +19,11 @@ import java.util.concurrent.TimeUnit;
  **/
 public class CustomerThreadPool {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService customThreadPool = customThreadPool();
-        customThreadPool.execute(()-> System.out.println(Thread.currentThread().getName()));
+        Future future = customThreadPool.submit(()->
+            System.out.println(Thread.currentThread().getName()));
+        System.out.println(future.get());
     }
 
     /**
@@ -34,15 +38,16 @@ public class CustomerThreadPool {
         int corePoolSize = 10;
         //最大线程数
         int maximumPoolSize = 10;
-        //
+        //非核心线程的最长存活时间（非核心线程多长时间后未执行任务就进行销毁）
         long keepAliveTime = 0;
         BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>();
         String poolNamePrefix = "自定义线程池";
         String threadNamePrefix = "线程名称前缀";
         ThreadFactory threadFactory = new MyFactory(poolNamePrefix,threadNamePrefix);
         ExecutorService customThreadPool = new ThreadPoolExecutor(corePoolSize,maximumPoolSize,
-                keepAliveTime,TimeUnit.SECONDS,workQueue);
+                keepAliveTime,TimeUnit.SECONDS,workQueue,threadFactory);
 
         return customThreadPool;
     }
+
 }
