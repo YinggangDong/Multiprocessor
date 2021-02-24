9e2e5cef26ecf9d7ea8a3a297118730c6145e909
YinggangDong <believerD@aliyun.com>
2021-02-24 19:08:18 +0800

    feat：完善对象大小估算

--- src/main/java/cn/dyg/keyword/jol/ObjectSize.java
+++ src/main/java/cn/dyg/keyword/jol/ObjectSize.java
@@ -21,11 +21,28 @@ public class ObjectSize {
 
 
 class A {
+    /**
+     * long对象 8个字节
+     */
     private long l;
 }
 
 class B {
-    private double s;
-    private int i;
 
+    /**
+     * 1个字节,会补对应字节
+     */
+    private boolean flag;
+    /**
+     * 调整后,会作为对象头12个字节后的4个字节
+     */
+    private int i;
+    /**
+     * string只有引用，若开启指针压缩会占用4个字节，否则占用8个字节
+     */
+    private String j;
+    /**
+     * 只有A的引用a,若开启指针压缩会占用4个字节，否则占用8个字节
+     */
+    private A a;
 }
