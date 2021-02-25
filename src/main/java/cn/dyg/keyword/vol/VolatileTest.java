8c1022a8909a996e2f6b464c92d46d9efba9d98f
YinggangDong <believerD@aliyun.com>
2021-02-25 19:39:24 +0800

    feat:volatile 的相关测试

---
+++ src/main/java/cn/dyg/keyword/vol/VolatileTest.java
@@ -0,0 +1,39 @@
+package cn.dyg.keyword.vol;
+
+import cn.dyg.util.ThreadUtil;
+
+import java.util.concurrent.ExecutorService;
+
+/**
+ * VolatileTest 类是 关键字 volatile 的相关测试
+ *
+ * @author dongyinggang
+ * @date 2021-02-25 16:56
+ **/
+public class VolatileTest {
+
+    private volatile int volObj = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        VolatileTest volatileTest = new VolatileTest();
+        ExecutorService executorService = ThreadUtil.buildThreadExecutor(2);
+        for (int i = 0; i < 20; i++) {
+            executorService.execute(volatileTest::increase);
+        }
+
+        Thread.sleep(3000);
+        //结果很大可能不为200000
+        System.out.println(volatileTest.volObj);
+        executorService.shutdown();
+
+    }
+
+    private void increase() {
+        for (int i = 0; i < 10000; i++) {
+            volObj++;
+        }
+        System.out.println("线程结束");
+    }
+
+
+}
