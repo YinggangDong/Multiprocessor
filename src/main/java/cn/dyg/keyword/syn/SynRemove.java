35f5b528b0d4b1de1ae3e0508eca4a933f0842b4
YinggangDong <believerD@aliyun.com>
2021-03-05 17:26:41 +0800

    feat:测试锁消除

--- src/main/java/cn/dyg/keyword/syn/SynRemove.java
+++ src/main/java/cn/dyg/keyword/syn/SynRemove.java
@@ -10,6 +10,7 @@ public class SynRemove {
 
     public static void main(String[] args) {
         synRemoveTest();
+        synRemoveTest1();
     }
 
     /**
@@ -18,9 +19,28 @@ public class SynRemove {
      * 因此实际会被优化，执行锁消除，和没有 synchronized 的代码效率一样高
      */
     private static void synRemoveTest() {
+        long start = System.currentTimeMillis();
         Object obj = new Object();
-        synchronized (obj) {
-            System.out.println("no monitor enter");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 100000; i++) {
+            synchronized (obj) {
+                sb.append(i);
+            }
         }
+        System.out.println("有 synchronized 循环结束,耗时"
+                + (System.currentTimeMillis() - start) + "ms");
+
+    }
+
+    private static void synRemoveTest1() {
+        long start = System.currentTimeMillis();
+        Object obj = new Object();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 100000; i++) {
+            sb.append(i);
+        }
+        System.out.println("无 synchronized 循环结束,耗时"
+                + (System.currentTimeMillis() - start) + "ms");
+
     }
 }
