75b51513f9e64ac22d1990861f4994f79da8c371
YinggangDong <believerD@aliyun.com>
2021-09-10 09:52:05 +0800

    feat:计算对象大小

--- src/main/java/cn/dyg/keyword/jol/JavaObjectLayoutTest.java
+++ src/main/java/cn/dyg/keyword/jol/JavaObjectLayoutTest.java
@@ -1,6 +1,11 @@
 package cn.dyg.keyword.jol;
 
+import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
 import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.info.GraphLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * JavaObjectLayoutTest 类是 jol java对象
@@ -14,7 +19,8 @@ public class JavaObjectLayoutTest {
         //锁对对象头信息的变更
 //        testLockFlag();
         //数组对象头信息
-        arrayObjectHeader();
+//        arrayObjectHeader();
+        test();
     }
 
     /**
@@ -42,6 +48,30 @@ public class JavaObjectLayoutTest {
         String[] arrays = new String[258];
         arrays[0] = "hello world";
         System.out.println(ClassLayout.parseInstance(arrays).toPrintable());
+    }
+
+    private static void test() {
+        WorkOrderPO wr = new WorkOrderPO();
+        wr.setWrCode("ZXGD202108210001");
+//        System.out.println(GraphLayout.parseInstance("ZXGD202108210001").toPrintable());
+//        System.out.println(ObjectSizeCalculator.getObjectSize(wr));
+//        System.out.println(GraphLayout.parseInstance(wr).toPrintable());
+        List<WorkOrderPO> list = new ArrayList<>();
+        list.add(wr);
+        System.out.println(ObjectSizeCalculator.getObjectSize(list));
+        for (int i = 0; i < 99; i++) {
+            WorkOrderPO wr1 = new WorkOrderPO();
+            wr1.setWrCode("ZXGD202108240000" + i);
+            list.add(wr1);
+            if (i == 98) {
+                System.out.println(GraphLayout.parseInstance(wr1).toPrintable());
+                System.out.println(ObjectSizeCalculator.getObjectSize("ZXGD202108240000"));
+                System.out.println(ObjectSizeCalculator.getObjectSize("ZXGD202108240000" + i));
+            }
+        }
+        System.out.println(ObjectSizeCalculator.getObjectSize(list));
+//        System.out.println(GraphLayout.parseInstance(list).toPrintable());
+
     }
 }
 
---
+++ src/main/java/cn/dyg/keyword/jol/WorkOrderPO.java
@@ -0,0 +1,15 @@
+package cn.dyg.keyword.jol;
+
+import lombok.Data;
+
+/**
+ * @ClassName WorkOrderpo
+ * @Description 类描述
+ * @Author CAOSHAOSHUAI
+ * @Date 2020/7/1 0001 上午 10:15
+ * @Version 1.0
+ **/
+@Data
+public class WorkOrderPO {
+    private String wrCode;
+}
